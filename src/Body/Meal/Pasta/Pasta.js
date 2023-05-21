1fa2aedaccf7a5004104069ded91463e3696178d
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2023-05-21 23:15:22 +0330

    Add files via upload

---
+++ src/Body/Meal/Pasta/Pasta.js
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import MyContext from "../../../MyContext";
+import MenuItem from "../../MenuItem/MenuItem";
+import pastaPhoto from "./img/noodles-spaguetti-svgrepo-com.svg";
+import "./pasta.css";
+export default function Pasta() {
+    const {pastas, order, flexType,titleBg } = useContext(MyContext);
+    function order_beafAlfredo() {
+        order(pastas.beafAlfredo.name, pastas.beafAlfredo.price)
+    }
+    function order_chickenAlfredo() {
+        order(pastas.chickenAlfredo.name, pastas.chickenAlfredo.price)
+    }
+    return (
+        <div className="item_wall pasta_wall">
+            <div className={titleBg+" title"}>
+                <img src={pastaPhoto} className="icon" alt="pasta icon" />
+                پاستا
+            </div>
+            <div className={flexType}>
+                <MenuItem
+                    picClass="beafAlfredo"
+                    itemName={pastas.beafAlfredo.name}
+                    price={pastas.beafAlfredo.price}
+                    ingredients={pastas.beafAlfredo.ingredients}
+                    onClick={order_beafAlfredo} />
+                <MenuItem
+                    picClass="chickenAlfredo"
+                    itemName={pastas.chickenAlfredo.name}
+                    price={pastas.chickenAlfredo.price}
+                    ingredients={pastas.chickenAlfredo.ingredients}
+                    onClick={order_chickenAlfredo} />
+            </div>
+        </div>
+    )
+}
