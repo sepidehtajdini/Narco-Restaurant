8f369e0b48535612e0cf6367ef2d739ff09d7863
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2024-02-29 22:14:44 +0330

    Add files via upload

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import MyContext from "../MyContext";
 import "./footer.css";
-import { ReactComponent as Top } from "./arrow-alt-circle-up.svg";
+import { ReactComponent as Top } from "../img/svgs/arrow-alt-circle-up.svg";
 export default function Footer() {
     const { titleBg } = useContext(MyContext);
     return (
         <div className={titleBg + " restaurant_footer"}>
-            کلیه حقوق مادی و معنوی این سایت مربوط به سپیده تاجدینی می‌باشد
-            <a href="#">
-                <Top style={{ fill: "red" }} />
+            کلیه حقوق مادی و معنوی این سایت متعلق به سپیده تاجدینی می‌باشد
+            <a href="#breakfast">
+                <Top style={{ fill: "red"}} />
             </a>
         </div>
     )
