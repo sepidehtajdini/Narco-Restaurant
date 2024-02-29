a66e9902d0c6879ba41347202807bd50bdde6bf5
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2024-02-29 22:08:55 +0330

    Add files via upload

--- src/Body/Gallery/Gallery.js
+++ src/Body/Gallery/Gallery.js
@@ -1,41 +1,15 @@
 import { useContext, useState } from "react";
 import MyContext from "../../MyContext";
-import img1 from "./img/img1.jpeg";
-import img2 from "./img/img2.jpg";
-import img3 from "./img/img3.jpg";
-import img4 from "./img/img4.jpeg";
-import galleryPhoto from "../../Header/img/gallery-orange.svg";
+import img1 from "../../img/photos/img1.jpeg";
+import img2 from "../../img/photos/img2.jpg";
+import img3 from "../../img/photos/img3.jpg";
+import img4 from "../../img/photos/img4.jpeg";
+import galleryPhoto from "../../img/svgs/gallery-orange.svg";
 import "./gallery.css";
 export default function Gallery({ id }) {
     const { titleBg } = useContext(MyContext);
-    const [big_img1, setBig_img1] = useState("hide");
-    const [big_img2, setBig_img2] = useState("hide");
-    const [big_img3, setBig_img3] = useState("hide");
-    const [big_img4, setBig_img4] = useState("hide");
-    function selectImg1() {
-        setBig_img2("hide");
-        setBig_img3("hide");
-        setBig_img4("hide");
-        setBig_img1("big_img");
-    }
-    function selectImg2() {
-        setBig_img1("hide");
-        setBig_img3("hide");
-        setBig_img4("hide");
-        setBig_img2("big_img");
-    }
-    function selectImg3() {
-        setBig_img1("hide");
-        setBig_img2("hide");
-        setBig_img4("hide");
-        setBig_img3("big_img");
-    }
-    function selectImg4() {
-        setBig_img1("hide");
-        setBig_img2("hide");
-        setBig_img3("hide");
-        setBig_img4("big_img");
-    }
+    const [bigImg, setBigImg] = useState("hide");
+
     return (
         <div id={id}>
             <div className={titleBg + ' title'}>
@@ -43,17 +17,28 @@ export default function Gallery({ id }) {
                 تصاویر
             </div>
             <div className="small_img_div">
-                <img src={img1} alt="narco restaurant" className='small_img' onClick={selectImg1} />
-                <img src={img2} alt="narco restaurant" className='small_img' onClick={selectImg2} />
-                <img src={img3} alt="narco food" className='small_img' onClick={selectImg3} />
-                <img src={img4} alt="narco food" className='small_img' onClick={selectImg4} />
+                <img
+                    src={img1}
+                    alt="narco restaurant"
+                    className='small_img'
+                    onClick={() => setBigImg("big_img_div big_img1")} />
+                <img
+                    src={img2}
+                    alt="narco restaurant"
+                    className='small_img'
+                    onClick={() => setBigImg("big_img_div big_img2")} />
+                <img
+                    src={img3}
+                    alt="narco food"
+                    className='small_img'
+                    onClick={() => setBigImg("big_img_div big_img3")} />
+                <img
+                    src={img4}
+                    alt="narco food"
+                    className='small_img'
+                    onClick={() => setBigImg("big_img_div big_img4")} />
             </div>
-            <div className="big_img_div">
-                <img src={img1} alt="narco restaurant" className={big_img1} />
-                <img src={img2} alt="narco restaurant" className={big_img2} />
-                <img src={img3} alt="narco food" className={big_img3} />
-                <img src={img4} alt="narco food" className={big_img4} />
-            </div>
+            <div className={bigImg} />
         </div >
     );
 }
